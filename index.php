a34ef0689048f42c1690f41a0d1aa6d2ac6a86ab
danielabrantes <daniel.abrantes@gmail.com>
2011-05-09 14:53:30 +0100

    'separação dos templates para resolver problemas de cache'

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 require_once('common/common.php');
 if(isset($language))
 {
-	$smarty->display('index.tpl');
+	//$smarty->display('index.tpl');
+	$view->display($language);
 }
 else{
 	$smarty->display('bandeiras.tpl');
